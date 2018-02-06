6dfcca5a45f3abc943d375c50e089c450b815033
Ivan Zotov <tridcatov@gmail.com>
2018-02-06 16:21:50 +0300

    Refactoring types to posix types

--- src/rpl-types.h
+++ src/rpl-types.h
@@ -1,11 +1,13 @@
 #ifndef RPL_TYPES_H
 #define RPL_TYPES_H
 
+#include <inttypes.h>
 
+/*
 typedef unsigned int				uint32_t;
 typedef short unsigned int 	uint16_t;
 typedef unsigned char			uint8_t;
 typedef short int int16_t;
 typedef char int8_t;
-
+*/
 #endif // RPL_TYPES_H
--- src/uip.h
+++ src/uip.h
@@ -1,6 +1,8 @@
 #ifndef UIP_H
 #define UIP_H
 
+#include "rpl-types.h"
+
 typedef union uip_ip6addr_t {
   uint8_t u8[16];
   uint16_t u16[8];
